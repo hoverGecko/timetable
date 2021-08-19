324c74033115991eb5e84400b7cf0de26675051b
hoverGecko <46785140+hoverGecko@users.noreply.github.com>
2021-08-19 17:27:34 +0000

    Add files via upload

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,7 +1,7 @@
 const BottomBar = () => {
     return (
         <div className="BottomBar">
-            <a href="https://github.com/hoverGecko/timetable">github.com/hoverGecko/timetable</a>. v0.1.1. 2021/8/19. Inspired by CUTS.
+            <a href="https://github.com/hoverGecko/timetable">github.com/hoverGecko/timetable</a>. v0.1.1. 2021/8/20. Inspired by CUTS.
         </div>
     );
 }
